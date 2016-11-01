8eca556be37290ea134febeda2b058fe99d973ae
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-01 19:18:13 +0300

    Create directive ChildForm and template for him. Add some css

---
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/ChildFormDirective.js
@@ -0,0 +1,13 @@
+﻿app.directive("dirChildForm", function () {
+    return {
+        replace: false,
+        restrict: "A",
+        scope: {
+            child: "=child",
+            Cancel: "=cancel",
+            nums: "=nums",
+            Submit: "=submit"
+        },
+        templateUrl: "/scripts/crudApp/Directives/Templates/ChildForm.html"
+    };
+});
