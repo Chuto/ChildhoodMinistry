3bdbf12cfc26d46811bb7216671c03781d3dceca
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-01 18:22:25 +0300

    Create a Pagination directive with template

---
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/PaginationDirective.js
@@ -0,0 +1,11 @@
+﻿app.directive("dirPagination", function () {
+    return {
+        replace: false,
+        restrict: "A",
+        scope: {
+            setPage: "=loadPage",
+            pager: "=page"
+        },
+        templateUrl: "/scripts/crudApp/Directives/Templates/Pagination.html",
+    };
+});
