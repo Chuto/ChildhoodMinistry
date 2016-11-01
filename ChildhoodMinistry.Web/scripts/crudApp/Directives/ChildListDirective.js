7e1ef2c6455afbdbf6bf58c2b1a986a15529027b
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-01 17:56:39 +0300

    Add ChildList directive with template which show a list of children

---
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/ChildListDirective.js
@@ -0,0 +1,12 @@
+﻿app.directive("dirChildList", function () {
+    return {
+        replace: false,
+        restrict: "A",
+        scope: {
+            children: "=children",
+            Edit: "=editFunc",
+            Delete: "=deleteFunc"
+        },
+        templateUrl: "/scripts/crudApp/Directives/Templates/ChildList.html"
+    };
+});
