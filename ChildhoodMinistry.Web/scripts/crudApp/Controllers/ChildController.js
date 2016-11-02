0426263972ebc367c3ec4dadbc8da771f7476f39
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-02 19:22:47 +0300

    Moved pagination logic from ChildController.js to directive

--- ChildhoodMinistry.Web/scripts/crudApp/Controllers/ChildController.js
+++ ChildhoodMinistry.Web/scripts/crudApp/Controllers/ChildController.js
@@ -1,65 +1,25 @@
-﻿app.controller("ChildCRUDCtrl", function ($scope, crudChildService) {
-
+﻿app.controller("ChildCRUDCtrl", function ($window, $scope, crudChildService) {
+    $scope.ready = false;
     $scope.Submit;
 
-    $scope.pager = {};
     $scope.setPage = function (page) {
-        if (page < 1 || page > $scope.pager.totalPages) {
-            page = 1;
+        if (page < 1 || page > $scope.totalPages) {
+            return;
         }
-        var currentPage = page || 1;
         var pageSize = 2;
-        $scope.edit = false;
-        $scope.save = false;
         $scope.divEdit = false;
-        var getData = crudChildService.GetPageOfChild(currentPage, pageSize);
+        var getData = crudChildService.GetPageOfChild(page || 1, pageSize);
         getData.then(function (respon) {
             $scope.children = respon.data.Data;
-            currentPage = respon.data.Page.CurrentPage;
-            pageSize = respon.data.Page.PageSize;
-            var totalItems = respon.data.Page.TotalItems;
-            var totalPages = respon.data.Page.TotalPages;
-
-            var startPage, endPage;
-
-            if (totalPages <= 10) {
-                startPage = 1;
-                endPage = totalPages;
-            } else {
-                if (currentPage <= 6) {
-                    startPage = 1;
-                    endPage = 10;
-                } else if (currentPage + 4 >= totalPages) {
-                    startPage = totalPages - 9;
-                    endPage = totalPages;
-                } else {
-                    startPage = currentPage - 5;
-                    endPage = currentPage + 4;
-                }
-            }
-
-            var startIndex = (currentPage - 1) * pageSize;
-            var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-            var pages = window._.range(startPage, endPage + 1);
-
-            $scope.pager = {
-                totalItems: totalItems,
-                currentPage: currentPage,
-                pageSize: pageSize,
-                totalPages: totalPages,
-                startPage: startPage,
-                endPage: endPage,
-                startIndex: startIndex,
-                endIndex: endIndex,
-                pages: pages
-            };
+            $scope.currentPage = respon.data.Page.CurrentPage;
+            $scope.totalPages = respon.data.Page.TotalPages;
+            $scope.ready = true;
         }, function () {
             alert('Ошибка чтения записи');
         });
     };
 
-    if ($scope.childhoodNum === undefined)
-        $scope.setPage(1);
+    $scope.setPage(1);
 
     function getChildhoodNum() {
         var getData = crudChildService.GetChildhoods();
@@ -73,8 +33,6 @@
     $scope.EditChild = function (child) {
         $scope.Submit = $scope.UpdateChild;
         getChildhoodNum();
-        $scope.edit = true;
-        $scope.save = false;
         var getData = crudChildService.GetChildById(child.Ind);
         getData.then(function (item) {
             $scope.child = item.data;
@@ -87,57 +45,43 @@
 
     $scope.AddChild = function () {
         var child = $scope.child;
-        child.ChildhoodId = $.grep($scope.nums, function (e) { return e.Number == child.ChildhoodNum })[0].Ind;
-        var getData = crudChildService.AddChild(child);
+        $scope.child.ChildhoodId = $.grep($scope.nums, function (e) { return e.Number == $scope.child.ChildhoodNum })[0].Ind;
+        var getData = crudChildService.AddChild($scope.child);
         getData.then(function (msg) {
-            if ($scope.childhoodNum)
-                $scope.GetChildrenList(parseInt($scope.childhoodNum));
-            else
-                $scope.setPage(1);
+            $window.location.href = '/Child';
             alert(msg.data);
             $scope.divEdit = false;
         }, function () {
             alert('Ошибка обновления записи');
         });
-        $scope.save = false;
     };
 
     $scope.UpdateChild = function () {
-        var child = $scope.child;
-        child.ChildhoodId = $.grep($scope.nums, function (e) { return e.Number == $scope.child.ChildhoodNum })[0].Ind;
-        var getData = crudChildService.UpdateChild(child);
+        $scope.child.ChildhoodId = $.grep($scope.nums, function (e) { return e.Number == $scope.child.ChildhoodNum })[0].Ind;
+        var getData = crudChildService.UpdateChild($scope.child);
         getData.then(function (msg) {
-            if ($scope.childhoodNum)
-                $scope.GetChildrenList(parseInt($scope.childhoodNum));
-            else
-                $scope.setPage(1);
+            $window.location.href = '/Child';
             alert(msg.data);
             $scope.divEdit = false;
         }, function () {
             alert('Ошибка добавления записи');
         });
-        $scope.edit = false;
     };
 
     $scope.AddChildDiv = function () {
         $scope.Submit = $scope.AddChild;
-        $scope.child = null;
+        $scope.child = {};
         getChildhoodNum();
-        $scope.edit = false;
-        $scope.save = true;
         $scope.Action = "Добавление";
         $scope.divEdit = true;
     };
 
-    $scope.DeleteChild = function (child) {
-        var getData = crudChildService.DeleteChild(child.Ind);
+    $scope.DeleteChild = function (Ind) {
+        var getData = crudChildService.DeleteChild(Ind);
         getData.then(function (msg) {
             alert(msg.data);
             $scope.divEdit = false;
-            if ($scope.childhoodNum)
-                $scope.GetChildrenList(parseInt($scope.childhoodNum));
-            else
-                $scope.setPage(1);
+            $window.location.href = '/Child';
         }, function () {
             alert('Ошибка удаления записи');
         });
--- ChildhoodMinistry.Web/scripts/crudApp/Directives/PaginationDirective.js
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/PaginationDirective.js
@@ -3,9 +3,37 @@
         replace: false,
         restrict: "A",
         scope: {
-            setPage: "=loadPage",
-            pager: "=page"
+            setPage: "&",
+            currentPage: "=",
+            totalPages: "="
         },
         templateUrl: "/scripts/crudApp/Directives/Templates/Pagination.html",
+        controller: function ($scope) {
+                var startPage, endPage;
+
+                if ($scope.totalPages <= 10) {
+                    startPage = 1;
+                    endPage = $scope.totalPages;
+                } else {
+                    if ($scope.currentPage <= 6) {
+                        startPage = 1;
+                        endPage = 10;
+                    } else if ($scope.currentPage + 4 >= $scope.totalPages) {
+                        startPage = $scope.totalPages - 9;
+                        endPage = $scope.totalPages;
+                    } else {
+                        startPage = $scope.currentPage - 5;
+                        endPage = $scope.currentPage + 4;
+                    }
+                }
+
+                var startIndex = ($scope.currentPage - 1) * $scope.PageSize;
+                var endIndex = Math.min(startIndex + $scope.PageSize - 1, $scope.TotalItems - 1);
+                $scope.pages = window._.range(startPage, endPage + 1);
+
+            $scope.LoadPage = function(num) {
+                $scope.setPage({ page: num })
+            };
+        }
     };
 });
