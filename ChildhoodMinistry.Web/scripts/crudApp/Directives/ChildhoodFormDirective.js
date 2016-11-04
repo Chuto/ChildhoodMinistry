9c5eca1886ac049ec73306b738e7874e4f70877b
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-04 14:43:29 +0300

    Create ChildhoodForm directive with template

---
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/ChildhoodFormDirective.js
@@ -0,0 +1,14 @@
+﻿app.directive("dirChildhoodForm", function () {
+    return {
+        replace: false,
+        restrict: "E",
+        scope: {
+            childhood: "=",
+            formAction: "&"
+        },
+        controller: function () { },
+        controllerAs: "vmForm",
+        bindToController: true,
+        templateUrl: "/scripts/crudApp/Directives/Templates/ChildhoodForm.html"
+    };
+});
