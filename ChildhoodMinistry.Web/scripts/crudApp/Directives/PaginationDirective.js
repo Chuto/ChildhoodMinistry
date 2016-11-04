37bbacc0975e1fa3305e5e968b553d6c2d562ff3
BELHARD\zelenkova <vitaliy.zelenko@oxagile.com>
2016-11-04 12:09:19 +0300

    Removed PaginationDirective.js. Create a function in pagination directive and give to directive as controller.

--- ChildhoodMinistry.Web/scripts/crudApp/Directives/PaginationDirective.js
+++ ChildhoodMinistry.Web/scripts/crudApp/Directives/PaginationDirective.js
@@ -1,4 +1,32 @@
-﻿app.directive("dirPagination", function () {
+﻿app.directive("dirPagination", function (_) {
+
+    var PagingController = function () {
+        var vmPage = this;
+        var startPage, endPage;
+
+        if (vmPage.totalPages <= 10) {
+            startPage = 1;
+            endPage = vmPage.totalPages;
+        } else {
+            if (vmPage.currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (vmPage.currentPage + 4 >= vmPage.totalPages) {
+                startPage = vmPage.totalPages - 9;
+                endPage = vmPage.totalPages;
+            } else {
+                startPage = vmPage.currentPage - 5;
+                endPage = vmPage.currentPage + 4;
+            }
+        }
+
+        vmPage.pages = _.range(startPage, endPage + 1);
+
+        vmPage.getPage = function (num) {
+            vmPage.loadPage({ page: num })
+        };
+    };
+
     return {
         replace: false,
         restrict: "E",
@@ -7,7 +35,7 @@
             currentPage: "=",
             totalPages: "="
         },
-        controller: "PagingController",
+        controller: ["_", PagingController],
         controllerAs: "vmPage",
         bindToController: true,
         templateUrl: "/scripts/crudApp/Directives/Templates/Pagination.html"
